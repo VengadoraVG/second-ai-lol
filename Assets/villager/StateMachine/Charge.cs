f2aea2f4ff3e7cb9d3af48785a7d1285aecc9c21
VG <vengadoravg@gmail.com>
2017-11-03 14:39:51 -0400

    charge follows target when it moves

--- Assets/villager/StateMachine/Charge.cs
+++ Assets/villager/StateMachine/Charge.cs
@@ -8,17 +8,27 @@ namespace Villager {
             public Attacker Owner;
 
             private NavMeshAgent _agent;
+            private bool _isActive = false;
 
             void Start () {
                 _agent = GetComponent<NavMeshAgent>();
             }
 
+            void Update () {
+                if (_isActive) {
+                    _agent.SetDestination(Owner.CurrentSpot.Position);
+                    transform.forward = transform.position - Owner.CurrentSpot.Owner.transform.position;
+                }
+            }
+
             public void EnterState () {
                 _agent.SetDestination(Owner.CurrentSpot.Position);
+                _isActive = true;
             }
 
             public void ExitState () {
                 _agent.ResetPath();
+                _isActive = false;
             }
         }
     }
---
+++ Assets/villager/StateMachine/MeleeAttack.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+namespace Villager {
+    namespace StateMachine {
+        public class MeleeAttack : MonoBehaviour, StateMachineBehaviour {
+            public void EnterState () {
+        
+            }
+
+            public void ExitState () {
+        
+            }
+        }
+    }
+}
